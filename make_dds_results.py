14656be381c143fa17903999c26bc059bae995d7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-07-19 12:28:11 +0900

    add test

--- make_dds_results.py
+++ make_dds_results.py
@@ -34,22 +34,37 @@ def random_to_pbn() -> str:
     return pbn
 
 
-def main(num):
+def to_dds(pbn):
     players = ["N", "E", "S", "W"]
     denominations = ["C", "D", "H", "S", "NT"]
+    dds_results = get_ddstable(pbn.encode('utf-8'))
+    dds = []
+    for player in players:
+        for denomination in denominations:
+            trick = dds_results[player][denomination]
+            dds.append(trick)
+    return ",".join([str(x) for x in dds])
+
+
+def test():
+    lines = """N:JT987.752.A3.J65 AQ542..J52.AT732 .T9863.T8764.K94 K63.AKQJ4.KQ9.Q8\t3,4,3,3,2,10,9,10,10,11,2,4,3,3,2,9,9,10,10,11
+N:8.J63.A982.QT982 97.Q9752.KJ5.K76 AQJ63.AK84.74.J5 KT542.T.QT63.A43\t8,7,7,7,6,5,6,5,6,7,7,7,7,6,6,5,6,5,6,7
+N:T43.K98.K843.876 Q72.Q4.T7652.A94 AJ98.T632.AJ9.52 K65.AJ75.Q.KQJT3\t4,6,5,6,5,9,7,7,7,7,3,6,5,6,5,9,6,7,6,7"""
+    for line in lines.split("\n"):
+        pbn, dds = line.split("\t")
+        assert to_dds(pbn) == dds, f"{to_dds(pbn)} != {dds}"
+
+
+def main(num):
     for i in tqdm(range(num)):
         pbn = random_to_pbn()
-        dds_results = get_ddstable(pbn.encode('utf-8'))
-        dds = []
-        for player in players:
-            for denomination in denominations:
-                trick = dds_results[player][denomination]
-                dds.append(trick)
-
-        print(pbn + "\t" + ",".join([str(x) for x in dds]))
+        dds = to_dds(pbn)
+        print(pbn + "\t" + dds)
 
 
 if __name__ == '__main__':
+    test()
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=None)
     parser.add_argument("--num", type=int, default=10_000_000)
